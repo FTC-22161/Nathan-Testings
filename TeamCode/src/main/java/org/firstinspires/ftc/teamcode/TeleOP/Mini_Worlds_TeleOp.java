66a675df28fcf38aaef113a03d175c75cd918815
rice-taco <naols2028@apps.mca.lwcc.org>
2025-05-20 16:58:04 -0500

    Mini_Worlds_TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/Mini_Worlds_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/Mini_Worlds_TeleOp.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
